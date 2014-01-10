9f05d6cd570565831fde7d5d7d45d4eed1da19bb
tangjuan <419608973@qq.com>
2014-01-10 18:06:56 +0800

    3

--- ReviewActivity.java
+++ ReviewActivity.java
@@ -13,8 +13,7 @@ import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;  
+  
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.view.animation.AlphaAnimation;
